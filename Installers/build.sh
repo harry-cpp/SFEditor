86378a4e08bacf7e24a37e2841fbf297d3406b53
cra0zy <cra0zy@gmail.com>
2015-10-04 13:52:03 +0200

    Updated Linux installer scripts

--- Installers/build.sh
+++ Installers/build.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+#build project
+xbuild /t:Build /p:Configuration=Release ../SFEditor/SFEditor.Linux.Gtk3/SFEditor.Linux.Gtk3.csproj
+cd ../SFEditor/SFEditor.Linux.Gtk3/bin/Release/
+./compile.sh
+cd ../../../../Installers/
+
+xbuild /t:Build /p:Configuration=Release ../SFEditor/SFEditor.Linux.Gtk2/SFEditor.Linux.Gtk2.csproj
+cd ../SFEditor/SFEditor.Linux.Gtk2/bin/Release/
+./compile.sh
+cd ../../../../Installers/
+
+#copy files to temp folder
 mkdir temp
 
 cp Linux/DEBIAN/. temp/DEBIAN/ -R
@@ -16,6 +28,7 @@ mkdir temp/usr/share/icons/gnome/scalable/mimetypes
 cp Linux/sf-application.svg temp/usr/share/icons/gnome/scalable/mimetypes/sf-application.svg
 cp Linux/sf-file.svg temp/usr/share/icons/gnome/scalable/mimetypes/sf-file.svg
 
+#build Gtk2 and Gtk3 linux packages
 mkdir temp/bin
 
 cp ../SFEditor/SFEditor.Linux.Gtk3/bin/Release/sfeditor temp/bin/sfeditor
@@ -26,4 +39,6 @@ rm temp/bin/sfeditor
 cp ../SFEditor/SFEditor.Linux.Gtk2/bin/Release/sfeditor temp/bin/sfeditor
 dpkg-deb --build temp sfeditor-gtk2.deb
 
+#cleanup
 rm -rf temp
+
